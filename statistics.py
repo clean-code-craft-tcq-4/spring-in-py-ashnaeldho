5a0c5dbfabae844f18546566a37a7f3e315a6261
ashnaeldho <ashnaeldho12@gmail.com>
2022-05-19 19:52:52 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,11 +1,11 @@
 
 def calculateStats(numbers):
-  len = len(numbers);
+  len1 = len(numbers);
   sum = 0;
   for sum in numbers:
     sum = sum + numbers;
     
-  avg = sum / len
+  avg = sum / len1;
   #avg = avg(numbers);
   max = max(numbers);
   min = min(numbers);
