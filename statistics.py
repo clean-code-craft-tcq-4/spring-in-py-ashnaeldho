7e805ac45b0f067916e2e00092173671ca1c2d08
ashnaeldho <ashnaeldho12@gmail.com>
2022-05-19 20:21:40 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,5 +1,5 @@
 #from statistics import mean
-import math
+#import math
 def calculateStats(numbers):
   length_1 = len(numbers);
   
@@ -7,12 +7,13 @@ def calculateStats(numbers):
   #for n in numbers:
   #  sum = sum + numbers[n];
     
-  for element in range(len(numbers)):
-        numbers[element]=float(numbers[element])
-        numbers[element]=math.pow(numbers[element],2)
-  total=sum(numbers[0:element+1])
-  avg = total/length_1; 
-  #avg = mean(numbers);
+  
+  #for element in range(len(numbers)):
+  #      numbers[element]=float(numbers[element])
+  #      numbers[element]=math.pow(numbers[element],2)
+  #total=sum(numbers[0:element+1])
+  #avg = total/length_1; 
+  avg = sum(numbers);
   max_num = max(numbers);
   min_num = min(numbers);
   return avg, max_num, min_num
