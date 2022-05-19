df90240d914ed81467985a5a26133c175a26f87b
ashnaeldho <ashnaeldho12@gmail.com>
2022-05-19 20:10:17 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,7 +1,11 @@
-from statistics import mean
+#from statistics import mean
 def calculateStats(numbers):
-  
-  avg = mean(numbers);
+  length_1 = len(numbers);
+  sum = 0;
+  for n in numbers:
+    sum = sum + numbers[n];
+  avg = sum/length_1; 
+  #avg = mean(numbers);
   max = max(numbers);
   min = min(numbers);
   return avg, max, min
