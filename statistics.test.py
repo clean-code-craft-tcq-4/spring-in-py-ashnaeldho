32edb298b8fa04eb60e049f42e0ddedae9ff7a73
ashnaeldho <ashnaeldho12@gmail.com>
2022-05-20 14:29:28 +0530

    Update statistics.test.py

--- statistics.test.py
+++ statistics.test.py
@@ -5,7 +5,7 @@ class StatsTest(unittest.TestCase):
   def test_report_min_max_avg(self):
     computedStats = statistics.calculateStats([1.5, 8.9, 3.2, 4.5])
     epsilon = 0.001
-    self.assertAlmostEqual(computedStats["avg"], 4.525, delta=epsilon)
+    self.assertAlmostEqual(computedStats["avg[0]"], 4.525, delta=epsilon)
     self.assertAlmostEqual(computedStats["max"], 8.9, delta=epsilon)
     self.assertAlmostEqual(computedStats["min"], 1.5, delta=epsilon)
 
