b2419e87f2bb64299dafd12bf4703e29909dc5ae
ashnaeldho <ashnaeldho12@gmail.com>
2022-05-20 14:39:24 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -2,18 +2,18 @@
 #import math
 def calculateStats(numbers):
   length_1 = len(numbers);
-  total = 0;
-  if type(numbers) == list:
+  #total = 0;
+  #if type(numbers) == list:
 
-    print("a is a list")
+    #print("a is a list")
 
-  else:
+  #else:
 
-    print("a is a tuple")
+    #print("a is a tuple")
     
   print(numbers)
-  for ele in range(0, len(numbers)):
-    total = total + numbers[ele]
+  #for ele in range(0, len(numbers)):
+    #total = total + numbers[ele]
   #sum = 0;
   #for n in numbers:
   #  sum = sum + numbers[n];
@@ -23,9 +23,11 @@ def calculateStats(numbers):
   #      numbers[element]=float(numbers[element])
   #      numbers[element]=math.pow(numbers[element],2)
   #total=sum(numbers[0:element+1])
-  avg = total/length_1; 
+  #avg = total/length_1; 
+  total = sum(map(float,numbers));
+  avg = total/length_1;
   print("average :", avg);
-  print("index of average : ", avg.index);
+  #print("index of average : ", avg.index);
   #avg = sum(numbers);
   #max_num = max(numbers);
   #min_num = min(numbers);
